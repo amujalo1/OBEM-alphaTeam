a77b29f7ad548521e5af0ee654fbb96a05f69afc
Safet Čomor <115508485+scomor55@users.noreply.github.com>
2025-02-24 14:16:55 +0100

    Device model added

---
+++ OBEM/OBEM/models/Device.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBEM.models
+{
+    class Device
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int LowerBound { get; set; }
+        public int UpperBound { get; set; }
+
+        public int NumericValue { get; set; }
+        
+        public string Unit { get; set; }
+
+        public string ApartmetName { get; set; }
+
+        public string Type { get; set; }
+
+        public string Floor { get; set; }
+
+
+    }
+}
