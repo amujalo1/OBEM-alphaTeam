9f8efa6d3932a5d05db326ab11e378293e0f8973
Anes Ćenanović <acenanovic1@etf.unsa.ba>
2025-02-25 11:23:39 +0100

    Kostur Zgrada UI-a

---
+++ OBEM/OBEM/Views/UnitDetails.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OBEM.Views
+{
+    /// <summary>
+    /// Interaction logic for UnitDetails.xaml
+    /// </summary>
+    public partial class UnitDetails : Page
+    {
+        public UnitDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
