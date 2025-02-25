d19e450dd4de6a495ce35acd2ef2f787f96a0aa7
Safet Čomor <115508485+scomor55@users.noreply.github.com>
2025-02-25 13:46:51 +0100

    trendingInfoId and records models added

---
+++ OBEM/OBEM/models/Record.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OBEM.models
+{
+    public class Record
+    {
+        [JsonProperty("Value", NullValueHandling = NullValueHandling.Ignore)]
+        public double Value { get; set; }
+
+        [JsonProperty("Time", NullValueHandling = NullValueHandling.Ignore)]
+        public string Time { get; set; }
+        [JsonProperty("Status", NullValueHandling = NullValueHandling.Ignore)]
+        public int Satus { get; set; }
+    }
+}
---
+++ OBEM/OBEM/models/TrendingInfoId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace OBEM.models
+{
+    public class TrendingInfoId
+    {
+        [JsonProperty("Result", NullValueHandling = NullValueHandling.Ignore)]
+        public int Result { get; set; }
+
+        [JsonProperty("Message", NullValueHandling = NullValueHandling.Ignore)]
+        public string Message { get; set; }
+
+        [JsonProperty("Values", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> Values { get; set; }
+
+        [JsonProperty("ColumnNames", NullValueHandling = NullValueHandling.Ignore)]
+        public List<string> ColumnNames { get; set; }
+        
+        [JsonProperty("Records", NullValueHandling = NullValueHandling.Ignore)]
+        public Dictionary<string, List<Record>> Records { get; set; }
+
+
+    }
+}
